d118e05fe7c709dc4a91a6b38bcc816561f0e787
Edwinrr31 <Edwinrr31@gmail.com>
2020-02-25 01:03:24 -0400

    Commit Inicial

---
+++ Tarea 4/App_Start/RouteConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Tarea_4
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Persona", action = "Index", id = UrlParameter.Optional }
+
+                /*El parametro "URL" especifica el patron que debe de tener la URL en este caso espeficica
+                 que controlador y que vista se invocaran en el caso de que el usuario no lo indique */
+            );
+        }
+    }
+}
---
+++ Tarea 4/Controllers/PersonaController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tarea_4.Models;
+namespace Tarea_4.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        [HttpPost]
+        public ActionResult Persona(Persona perso, HttpPostedFileBase imagen)
+        {
+            Persona obj = new Persona();
+            obj.cedula = Convert.ToInt64(Request.Form["cedula"]);
+            obj.nombre = Request.Form["nombre"].ToString();
+            obj.apellido = Request.Form["apellido"].ToString();
+            obj.edad = Convert.ToInt32(Request.Form["edad"]);
+            obj.telefono = Convert.ToInt64(Request.Form["telefono"]);
+            obj.correo = Request.Form["correo"].ToString();
+            obj.genero = Request.Form["genero"].ToString();
+            obj.cargo = Request.Form["cargo"].ToString();
+            if (ModelState.IsValid)
+            {
+                string nombreFoto = imagen.FileName;
+                imagen.SaveAs(Server.MapPath("/imagen/" + nombreFoto));
+
+                ViewBag.foto = nombreFoto;
+                return View("Index");
+            }
+            else
+            {
+                return View("Index");
+            }
+            
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
